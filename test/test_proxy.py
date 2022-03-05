78c60b77b29eec49fa036f8ad19fd02059c9ee43
Robert Lucey <robert@glimpse.ie>
2022-03-05 19:51:49 +0000

    Fix test name

--- test/test_proxy.py
+++ test/test_proxy.py
@@ -6,7 +6,7 @@ from eircode.proxy import Proxy
 class ProxyTest(TestCase):
 
     @skip('if no aws creds just skip')
-    def test_(self):
+    def test_proxy(self):
         proxy = Proxy(site='https://api.ipify.org')
 
         first_request = proxy.get('https://api.ipify.org')
